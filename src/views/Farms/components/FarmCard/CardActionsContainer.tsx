270790dd1caf735131671776147f170c4a56da72
muutualswap <75495839+muutualswap@users.noreply.github.com>
2022-01-27 15:19:49 -0500

    Update CardActionsContainer.tsx

--- src/views/Farms/components/FarmCard/CardActionsContainer.tsx
+++ src/views/Farms/components/FarmCard/CardActionsContainer.tsx
@@ -1,4 +1,4 @@
-import { Button, Flex, Text } from '@pancakeswap/uikit'
+import { Button, Flex, Text } from 'uikit'
 import BigNumber from 'bignumber.js'
 import ConnectWalletButton from 'components/ConnectWalletButton'
 import { ToastDescriptionWithTx } from 'components/Toast'
@@ -96,7 +96,7 @@ const CardActions: React.FC<FarmCardActionsProps> = ({ farm, account, addLiquidi
     <Action>
       <Flex>
         <Text bold textTransform="uppercase" color="secondary" fontSize="12px" pr="4px">
-          CAKE
+          BGSP
         </Text>
         <Text bold textTransform="uppercase" color="textSubtle" fontSize="12px">
           {t('Earned')}

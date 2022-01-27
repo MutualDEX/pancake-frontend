a7d814889ffc597788a28673bd4252d2387c23a0
muutualswap <75495839+muutualswap@users.noreply.github.com>
2022-01-27 14:02:48 -0500

    Update BountyModal.tsx

--- src/views/Pools/components/BountyModal.tsx
+++ src/views/Pools/components/BountyModal.tsx
@@ -12,7 +12,7 @@ import {
   useTooltip,
   Message,
   MessageText,
-} from '@pancakeswap/uikit'
+} from 'uikit'
 import { getBalanceNumber } from 'utils/formatBalance'
 import { useCakeVaultContract } from 'hooks/useContract'
 import useTheme from 'hooks/useTheme'
@@ -101,7 +101,7 @@ const BountyModal: React.FC<BountyModalProps> = ({ onDismiss, TooltipComponent }
       <Flex alignItems="flex-start" justifyContent="space-between">
         <Text>{t('You’ll claim')}</Text>
         <Flex flexDirection="column">
-          <Balance bold value={cakeBountyToDisplay} decimals={7} unit=" CAKE" />
+          <Balance bold value={cakeBountyToDisplay} decimals={7} unit=" BGSP" />
           <Text fontSize="12px" color="textSubtle">
             <Balance
               fontSize="12px"
@@ -119,7 +119,7 @@ const BountyModal: React.FC<BountyModalProps> = ({ onDismiss, TooltipComponent }
         <Text fontSize="14px" color="textSubtle">
           {t('Pool total pending yield')}
         </Text>
-        <Balance color="textSubtle" value={totalYieldToDisplay} unit=" CAKE" />
+        <Balance color="textSubtle" value={totalYieldToDisplay} unit=" BGSP" />
       </Flex>
       <Flex alignItems="center" justifyContent="space-between" mb="24px">
         <Text fontSize="14px" color="textSubtle">

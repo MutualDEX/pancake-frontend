c92af9ff038397a1fa543ccccaf72242f5b64e23
muutualswap <75495839+muutualswap@users.noreply.github.com>
2022-01-27 13:00:06 -0500

    Update Footer.tsx

--- src/views/Home/components/Footer.tsx
+++ src/views/Home/components/Footer.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Flex, Heading, Text, Link } from '@pancakeswap/uikit'
+import { Flex, Heading, Text, Link } from 'uikit'
 import { useTranslation } from 'contexts/Localization'
 import ConnectWalletButton from 'components/ConnectWalletButton'
 import Container from 'components/Layout/Container'
@@ -20,7 +20,6 @@ const BgWrapper = styled.div`
 const StyledSunburst = styled(SunburstSvg)`
   height: 350%;
   width: 350%;
-
   ${({ theme }) => theme.mediaQueries.xl} {
     height: 400%;
     width: 400%;
@@ -36,50 +35,7 @@ const Wrapper = styled(Flex)`
   overflow: hidden;
 `
 
-const FloatingPancakesWrapper = styled(Container)`
-  overflow: hidden;
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  top: 0;
-  left: 50%;
-  transform: translateX(-50%);
-  visibility: hidden;
 
-  ${({ theme }) => theme.mediaQueries.md} {
-    visibility: visible;
-  }
-`
-
-const TopLeftImgWrapper = styled(Flex)`
-  position: absolute;
-  left: 0;
-  top: 0;
-`
-
-const BottomRightImgWrapper = styled(Flex)`
-  position: absolute;
-  right: 0;
-  bottom: 0;
-`
-
-const topLeftImage = {
-  path: '/images/home/flying-pancakes/',
-  attributes: [
-    { src: '1-bottom', alt: 'Pancake flying on the bottom' },
-    { src: '1-left', alt: 'Pancake flying on the left' },
-    { src: '1-top', alt: 'Pancake flying on the top' },
-  ],
-}
-
-const bottomRightImage = {
-  path: '/images/home/flying-pancakes/',
-  attributes: [
-    { src: '2-bottom', alt: 'Pancake flying on the bottom' },
-    { src: '2-top', alt: 'Pancake flying on the top' },
-    { src: '2-right', alt: 'Pancake flying on the right' },
-  ],
-}
 
 const Footer = () => {
   const { t } = useTranslation()
@@ -92,29 +48,20 @@ const Footer = () => {
           <StyledSunburst />
         </Flex>
       </BgWrapper>
-      <FloatingPancakesWrapper>
-        <TopLeftImgWrapper>
-          <CompositeImage {...topLeftImage} maxHeight="256px" />
-        </TopLeftImgWrapper>
-        <BottomRightImgWrapper>
-          <CompositeImage {...bottomRightImage} maxHeight="256px" />
-        </BottomRightImgWrapper>
-      </FloatingPancakesWrapper>
       <Wrapper>
         <Heading mb="24px" scale="xl" color="white">
-          {t('Start in seconds.')}
+          {t('BigSwap Decentralized Wallet.')}
         </Heading>
         <Text textAlign="center" color="white">
-          {t('Connect your crypto wallet to start using the app in seconds.')}
+          {t('Connect in seconds.')}
         </Text>
         <Text mb="24px" bold color="white">
           {t('No registration needed.')}
         </Text>
 
-        <Link external href="https://docs.pancakeswap.finance/">
-          {t('Learn how to start')}
+        <Link color="white" external href="https://bigswap.xyz/">
+          {t('Try It')}
         </Link>
-        {!account && <ConnectWalletButton mt="24px" />}
       </Wrapper>
     </>
   )

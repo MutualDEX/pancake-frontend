6a35acf1c4edc1db984fc7b7a9328fd53aad38e1
muutualswap <75495839+muutualswap@users.noreply.github.com>
2022-01-27 16:13:43 -0500

    Update config.ts

--- src/views/Voting/config.ts
+++ src/views/Voting/config.ts
@@ -1,10 +1,8 @@
-export const PROPOSALS_TO_SHOW = 10
+export const PROPOSALS_TO_SHOW = 0
 export const ADMINS = [
-  '0x842B508681eE336E74600974B4623B709477d29D', // TARO
-  '0x977e0c1005dff8749f8cac22f4df0bd5f013d1a7', // CHUNGUS
-  '0x6eaf1b33b8672c5dc40ab8f4ba3a0111723126c7', // HOPS
+  '0xd380b03415D7A976c7F4F31e502145C396031Ab1', // BigSwap
 ].map((address) => address.toLowerCase())
-export const IPFS_GATEWAY = 'https://gateway.ipfs.io/ipfs'
-export const SNAPSHOT_VERSION = '0.1.3'
-export const PANCAKE_SPACE = 'cake.eth'
+export const IPFS_GATEWAY = ''
+export const SNAPSHOT_VERSION = ''
+export const PANCAKE_SPACE = ''
 export const VOTE_THRESHOLD = 10

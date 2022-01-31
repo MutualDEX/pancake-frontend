3eee92a564c8db5e1435def2c5471052b09c589b
MutualDEX <35112540+MutualDEX@users.noreply.github.com>
2022-01-30 21:40:39 -0500

    Update contracts.ts

--- src/config/constants/contracts.ts
+++ src/config/constants/contracts.ts
@@ -1,17 +1,16 @@
 export default {
   masterChef: {
-    97: '',
-    56: '0x65FE214382fD9e0d6cF054EeD65C150C88ed6834',
+    97: '0x1d32c2945C8FDCBc7156c553B7cEa4325a17f4f9',
+    56: '0x73feaa1eE314F8c655E354234017bE2193C9E24E',
   },
   sousChef: {
-    97: '',
+    97: '0xd3af5fe61dbaf8f73149bfcfa9fb653ff096029a',
     56: '0x6ab8463a4185b80905e05a9ff80a2d6b714b9e95',
   },
-    cakeVault: {
-    56: '0x999B072EEd7c671D01Dd3C8563a4890Ff7083203',
-    97: '',
+  lotteryV2: {
+    97: '0x5790c3534F30437641541a0FA04C992799602998',
+    56: '0x5aF6D33DE2ccEC94efb1bDF8f92Bd58085432d2c',
   },
-   // //////////////////////
   multiCall: {
     56: '0xfF6FD90A470Aaa0c1B8A54681746b07AcdFedc9B',
     97: '0x8F3273Fb89B075b1645095ABaC6ed17B2d4Bc576',
@@ -52,12 +51,12 @@ export default {
     56: '0x23c41D28A239dDCAABd1bb1deF8d057189510066',
     97: '0x24ec6962dbe874F6B67B5C50857565667fA0854F',
   },
-  lotteryV2: {
-    97: '0x5790c3534F30437641541a0FA04C992799602998',
-    56: '0x5aF6D33DE2ccEC94efb1bDF8f92Bd58085432d2c',
+  cakeVault: {
+    56: '0xa80240Eb5d7E05d3F250cF000eEc0891d00b51CC',
+    97: '',
   },
   ifoPool: {
-    56: '',
+    56: '0x1B2A2f6ed4A1401E8C73B4c2B6172455ce2f78E8',
     97: '',
   },
   predictions: {
@@ -85,7 +84,7 @@ export default {
     97: '',
   },
   farmAuction: {
-    56: '',
+    56: '0xb92Ab7c1edcb273AbA24b0656cEb3681654805D2',
     97: '0x3F9602593b4f7C67ab045DB51BbDEa94E40fA9Fe',
   },
   AnniversaryAchievement: {

7ceabcb8313f8e5e696cac6d0c06c860898dcbbd
MutualDEX <35112540+MutualDEX@users.noreply.github.com>
2022-02-03 13:36:38 -0500

    Update endpoints.ts

--- src/config/constants/endpoints.ts
+++ src/config/constants/endpoints.ts
@@ -13,7 +13,16 @@ export const BITQUERY_API = 'https://graphql.bitquery.io/ide/BigSwap-Pairs'
  * V1 will be deprecated but is still used to claim old rounds
  */
 export const GRAPH_API_PREDICTION_V1 = 'https://api.thegraph.com/subgraphs/name/pancakeswap/prediction'
-export const INFO_CLIENT = 'https://graphql.bitquery.io/ide/BigSwap-Pairs'  
+
+/**
+ * subgrafico de pancake de volumen y liquidez
+ * export const INFO_CLIENT = 'https://bsc.streamingfast.io/subgraphs/name/pancakeswap/exchange-v2'
+*/
+
+/**
+ * subgrafico de gol finance de volumen y liquidez
+ */
+export const INFO_CLIENT = 'https://api.thegraph.com/subgraphs/name/bigswap-finance/bigswap-finance-v2'
 export const BLOCKS_CLIENT = 'https://api.thegraph.com/subgraphs/name/pancakeswap/blocks'
 export const GRAPH_API_NFTMARKET = process.env.NEXT_PUBLIC_GRAPH_API_NFT_MARKET
 export const GRAPH_HEALTH = 'https://api.thegraph.com/index-node/graphql'

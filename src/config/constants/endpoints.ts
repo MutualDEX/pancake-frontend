914d04a9cda0b99df99ea93d9e32d0ecd29b6e47
MutualDEX <35112540+MutualDEX@users.noreply.github.com>
2022-02-14 23:04:34 -0500

    Update endpoints.ts

--- src/config/constants/endpoints.ts
+++ src/config/constants/endpoints.ts
@@ -7,7 +7,7 @@ export const API_PROFILE = process.env.NEXT_PUBLIC_API_PROFILE
 export const API_NFT = process.env.NEXT_PUBLIC_API_NFT
 export const SNAPSHOT_API = `${SNAPSHOT_BASE_URL}/graphql`
 export const SNAPSHOT_HUB_API = `${SNAPSHOT_BASE_URL}/api/message`
-export const BITQUERY_API = 'https://graphql.bitquery.io/ide/BigSwap-Pairs' 
+export const BITQUERY_API = 'https://graphql.bitquery.io/ide/Bigswap-exchange'   
 
 /**
  * V1 will be deprecated but is still used to claim old rounds
@@ -15,7 +15,7 @@ export const BITQUERY_API = 'https://graphql.bitquery.io/ide/BigSwap-Pairs'
 export const GRAPH_API_PREDICTION_V1 = 'https://api.thegraph.com/subgraphs/name/pancakeswap/prediction'
 
 
-export const INFO_CLIENT = 'https://bsc.streamingfast.io/subgraphs/name/pancakeswap/exchange-v2'
+export const INFO_CLIENT = 'https://graphql.bitquery.io/ide/Bigswap-exchange'  
 export const BLOCKS_CLIENT = 'https://api.thegraph.com/subgraphs/name/pancakeswap/blocks'
 export const GRAPH_API_NFTMARKET = process.env.NEXT_PUBLIC_GRAPH_API_NFT_MARKET
 export const GRAPH_HEALTH = 'https://api.thegraph.com/index-node/graphql'
